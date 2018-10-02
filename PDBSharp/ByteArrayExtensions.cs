7a8a3cae91ec5a67f0cdf45df7c03b690f0453f4
Stefano Moioli <smxdev4@gmail.com>
2018-10-03 01:37:39 +0200

    Rewritten ByteArrayExtensions.HexDump

--- PDBSharp/ByteArrayExtensions.cs
+++ PDBSharp/ByteArrayExtensions.cs
@@ -16,71 +16,60 @@ namespace Smx.PDBSharp
 {
 	public static class ByteArrayExtensions
 	{
-		/**
-		 * Source (Covered by CPOL license)
-		 * https://www.codeproject.com/Articles/36747/Quick-and-Dirty-HexDump-of-a-Byte-Array
-		 * https://www.codeproject.com/info/cpol10.aspx
-		 */
+		const int STRING_OFFSET = 62;
+
 		public static void HexDump(this byte[] bytes, int? optBytesLength = null) {
-			if(bytes == null) return;
-			int bytesPerLine = 16;
+			IEnumerable<byte> seq = bytes;
+			StringBuilder sb = new StringBuilder();
 
-			int bytesLength;
-			if(optBytesLength == null)
-				bytesLength = bytes.Length;
-			else
-				bytesLength = optBytesLength.Value;
+			int index = 0;
+			int lineLength = 0;
 
-			char[] HexChars = "0123456789ABCDEF".ToCharArray();
-
-			int firstHexColumn =
-				  8                   // 8 characters for the address
-				+ 3;                  // 3 spaces
-
-			int firstCharColumn = firstHexColumn
-				+ bytesPerLine * 3       // - 2 digit for the hexadecimal value and 1 space
-				+ (bytesPerLine - 1) / 8 // - 1 extra space every 8 characters from the 9th
-				+ 2;                  // 2 spaces 
-
-			int lineLength = firstCharColumn
-				+ bytesPerLine           // - characters to show the ascii value
-				+ Environment.NewLine.Length; // Carriage return and line feed (should normally be 2)
-
-			char[] line = (new String(' ', lineLength - Environment.NewLine.Length) + Environment.NewLine).ToCharArray();
-			int expectedLines = (bytesLength + bytesPerLine - 1) / bytesPerLine;
-			StringBuilder result = new StringBuilder(expectedLines * lineLength);
-
-			for(int i = 0; i < bytesLength; i += bytesPerLine) {
-				line[0] = HexChars[(i >> 28) & 0xF];
-				line[1] = HexChars[(i >> 24) & 0xF];
-				line[2] = HexChars[(i >> 20) & 0xF];
-				line[3] = HexChars[(i >> 16) & 0xF];
-				line[4] = HexChars[(i >> 12) & 0xF];
-				line[5] = HexChars[(i >> 8) & 0xF];
-				line[6] = HexChars[(i >> 4) & 0xF];
-				line[7] = HexChars[(i >> 0) & 0xF];
-
-				int hexColumn = firstHexColumn;
-				int charColumn = firstCharColumn;
-
-				for(int j = 0; j < bytesPerLine; j++) {
-					if(j > 0 && (j & 7) == 0) hexColumn++;
-					if(i + j >= bytesLength) {
-						line[hexColumn] = ' ';
-						line[hexColumn + 1] = ' ';
-						line[charColumn] = ' ';
-					} else {
-						byte b = bytes[i + j];
-						line[hexColumn] = HexChars[(b >> 4) & 0xF];
-						line[hexColumn + 1] = HexChars[b & 0xF];
-						line[charColumn] = (b < 32 ? '·' : (char)b);
-					}
-					hexColumn += 3;
-					charColumn++;
+			char[] lineBuf = new char[16];
+			int lineBufIdx = 0;
+			while(index < bytes.Length) {
+				if ((index % 16) == 0) {
+					sb.AppendLine();
+					sb.AppendFormat("{0:X8}   ", index);
+					lineLength = 8 + 3;
+					lineBufIdx = 0;
 				}
-				result.Append(line);
+
+				int takeLength = Math.Min(seq.Count(), 8);
+				var chunk = bytes.Skip(index).Take(takeLength);
+
+				chunk.All(b => {
+					char c = Convert.ToChar(b);
+					if (char.IsControl(c)) {
+						c = '.';
+					}
+					lineBuf[lineBufIdx++] = c;
+					lineBufIdx %= 16;
+
+					sb.AppendFormat("{0:X2} ", b);
+					lineLength += 3;
+
+					return true;
+				});
+				
+				seq = seq.Skip(takeLength);
+				index += takeLength;
+
+				if (takeLength == 8) {
+					sb.Append(" ");
+					lineLength++;
+				}
+
+				if ((index % 16) == 0 || index >= bytes.Length) {
+					sb.Append(new string(' ', STRING_OFFSET - lineLength));
+					int sz = index % 16;
+					if (sz == 0)
+						sz = 16;
+					sb.Append(lineBuf.Take(sz).ToArray());
+				}
 			}
-			Console.WriteLine(result.ToString());
+
+			Console.WriteLine(sb.ToString());
 		}
 	}
 }
