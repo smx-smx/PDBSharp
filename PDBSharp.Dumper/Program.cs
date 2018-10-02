02822a992639372c5957291b9fd8ac4fc4c60864
Stefano Moioli <smxdev4@gmail.com>
2018-10-02 20:38:12 +0200

    Splitted Library and Example Program (PDBSharp.Dumper)

---
+++ PDBSharp.Dumper/Program.cs
@@ -0,0 +1,28 @@
+﻿using Smx.PDBSharp;
+using System;
+using System.IO;
+
+namespace PDBSharp.Dumper
+{
+	class Program
+	{
+		static void Main(string[] args) {
+			if (args.Length < 1) {
+				Console.Error.WriteLine("Usage: [file.pdb]");
+				Environment.Exit(1);
+			}
+
+			var file = new FileStream(args[0], FileMode.Open, FileAccess.Read);
+			PDBFile pdb = new PDBFile(file);
+
+			foreach (var mod in pdb.Modules) {
+				foreach (var sym in mod.Symbols) {
+					Console.WriteLine($"[MAIN] => {sym.ToString()}");
+				}
+			}
+
+			Console.WriteLine("Press Enter to continue...");
+			Console.ReadLine();
+		}
+	}
+}
