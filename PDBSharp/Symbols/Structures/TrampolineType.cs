149d264608c7502b9cfc831c73952d1621eda9ce
Stefano Moioli <smxdev4@gmail.com>
2019-07-13 00:05:37 +0200

    Fixed copyright header

--- PDBSharp/Symbols/Structures/TrampolineType.cs
+++ PDBSharp/Symbols/Structures/TrampolineType.cs
@@ -1,11 +1,12 @@
-﻿#region License
+#region License
 /*
- * Copyright (C) 2018 Stefano Moioli <smxdev4@gmail.com>
+ * Copyright (C) 2019 Stefano Moioli <smxdev4@gmail.com>
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 #endregion
+
 using System;
 
 namespace Smx.PDBSharp.Symbols.Structures
