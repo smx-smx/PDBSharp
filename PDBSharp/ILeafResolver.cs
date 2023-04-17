90f056f3c9dc8fe1aba4883ebb3e140f1bd7333f
Stefano Moioli <smxdev4@gmail.com>
2023-04-18 00:32:29 +0200

    Updated Copyright

--- PDBSharp/ILeafResolver.cs
+++ PDBSharp/ILeafResolver.cs
@@ -1,3 +1,11 @@
+#region License
+/*
+ * Copyright (C) 2023 Stefano Moioli <smxdev4@gmail.com>
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+#endregion
 ﻿namespace Smx.PDBSharp.LeafResolver;
 
 public interface ILeafResolver
